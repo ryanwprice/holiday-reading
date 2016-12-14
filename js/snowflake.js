009945dbfacc8f3e7c5d81ed8bb28682acf948dd
ryanwprice <ryan@ryanprice.net>
2016-12-14 14:05:22 -0500

    rickroll

--- js/snowflake.js
+++ js/snowflake.js
@@ -8,7 +8,6 @@ var background_size = "";
 var newFlake = function(forCount) {
 	flake.x_pos = Math.floor(Math.random() * (90+ 1));
 	flake.y_pos = Math.floor(Math.random() * (90+ 1));
-	flake.rotation = Math.floor(Math.random() * (360));
 	flake.size = Math.floor(Math.random() * (20 - 5)+ 5);
 	flake.type = Math.floor(Math.random() * 9) + 1;
 	flakes[forCount] = flake;
@@ -22,12 +21,20 @@ for (i=0; i < flakeCount; i++ ){
 		background_pos = background_pos + ", " + flakes[i].x_pos + "% " + flakes[i].y_pos + "%";
 		background_size = background_size + ", " + flakes[i].size + "%";
 	} else {
-		background_image = background_image + "url(img/snowflake" + flakes[i].type + ".svg)";
-		background_pos = background_pos + flakes[i].x_pos + "% " + flakes[i].y_pos + "%";
-		background_size = background_size + flakes[i].size + "%";
+		background_image = "url(img/snowflake" + flakes[i].type + ".svg)";
+		background_pos = flakes[i].x_pos + "% " + flakes[i].y_pos + "%";
+		background_size = flakes[i].size + "%";
 	};
 };
 
 document.body.style.backgroundImage = background_image;
 document.body.style.backgroundPosition = background_pos;
 document.body.style.backgroundSize = background_size;
+
+
+
+
+
+
+
+
